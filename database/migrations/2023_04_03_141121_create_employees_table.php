c84f1fb8307c474ce3b20a12b74c48ba63d5d189
Anamul-09 <mdanamulislam09@gmail.com>
2023-04-04 13:47:38 +0600

    install project,create model ,migration and controller with resorchful,add table field and forienkey

---
+++ database/migrations/2023_04_03_141121_create_employees_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstname',30);
+            $table->string('lastname',30);
+            $table->string('address',150);
+            $table->unsignedBigInteger('department_id');
+            $table->unsignedBigInteger('city_id');
+            $table->unsignedBigInteger('state_id');
+            $table->unsignedBigInteger('country_id');
+            $table->string('zip_code',6);
+            $table->date('birthdate');
+            $table->date('date_hired');
+            $table->timestamps();
+
+
+            $table->foreign('country_id')->references('id')->on('countries')->onDelete('cascade');
+            $table->foreign('state_id')->references('id')->on('states')->onDelete('cascade');
+            $table->foreign('city_id')->references('id')->on('cities')->onDelete('cascade');
+            $table->foreign('department_id')->references('id')->on('departments')->onDelete('cascade');
+
+           
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
